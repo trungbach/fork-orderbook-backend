0cdac237ff14ec933218fd2399823d5eedd1c987
im6h <danghaiv.18@gmail.com>
2023-03-29 16:28:37 +0700

    fix: add custom query builder

--- src/repositories/postgre/candle.repository.ts
+++ src/repositories/postgre/candle.repository.ts
@@ -1,4 +1,38 @@
 import PostgresDB from 'src/config/postgres';
 import { Candle } from '../../entities/postgre/candle.entity';
 
-export const CandleRepository = PostgresDB.getRepository(Candle).extend({});
+export const CandleRepository = PostgresDB.getRepository(Candle).extend({
+  getLastCandleByProductId(productId: number, granularity: number) {
+    const lastCandle = this.createQueryBuilder('candle')
+      .where('candle.product_id = :product_id', { product_id: productId })
+      .andWhere('candle.granularity = :granularity', { granularity })
+      .orderBy('time DESC')
+      .limit(1)
+      .getOne();
+
+    return lastCandle;
+  },
+
+  getCandlesbyByProductId(
+    productId: number,
+    granularity: number,
+    limit?: number,
+    offset?: number,
+  ) {
+    let qb = this.createQueryBuilder('candle')
+      .where('candle.product_id = :product_id', { product_id: productId })
+      .andWhere('candle.granularity = :granularity', { granularity });
+
+    const count = qb.count();
+
+    if (limit) qb = qb.limit(limit);
+
+    if (offset) qb = qb.offset(offset);
+
+    const candles = qb.getMany();
+    return {
+      count,
+      candles,
+    };
+  },
+});
