2934d3418978636b609d67c891dd281eafb4be12
im6h <danghaiv.18@gmail.com>
2023-03-29 18:31:57 +0700

    add module to main app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,26 +6,30 @@ import { postgresConfig } from './config/postgres';
 import { BullModule } from '@nestjs/bull';
 import { CrawlRpcModule } from './modules/crawl-rpc/crawl-rpc.module';
 import { CommandModule } from './modules/only-cmd/command/command.module';
+import { WorkerModule } from './modules/worker/worker.module';
+import { ApiModule } from './modules/openapi/api.module';
 
 // import and provider run all commands and run server
 let importModules = [
   CrawlRpcModule,
   TypeOrmModule.forRoot(postgresConfig),
+  BullModule.forRoot({
+    redis: {
+      host: config.redis.host,
+      port: +config.redis.port,
+    },
+  }),
 ] as any[];
 
-if (config.isRunCmd) { // run command line
-  importModules = importModules.concat([
-    CommandModule
-  ]);
-} else { // run web
+if (config.isRunCmd) {
+  // run command line
+  importModules = importModules.concat([CommandModule]);
+} else {
+  // run web
   importModules = importModules.concat([
     ScheduleModule.forRoot(),
-    BullModule.forRoot({
-      redis: {
-        host: config.redis.host,
-        port: +config.redis.port,
-      },
-    }),
+    WorkerModule,
+    ApiModule,
   ]);
 }
 
