0c432b3486a992b1674e11d085218c11571bce22
Giang Soda <giang.nt@orai.io>
2023-04-03 17:03:29 +0700

    cors site oriign

--- src/config/cros.ts
+++ src/config/cros.ts
@@ -1,8 +1,8 @@
 import config from '../config';
 
 const sitesEnv = {
-  dev: ['http://localhost:3000', 'https://oraidex.io'],
-  prod: ['https://oraidex.io'],
+  dev: ['http://localhost:3000', '^(https://([^.]*.)?oraidex.io)$'],
+  prod: ['^(https://([^.]*.)?oraidex.io)$'],
 };
 
 const CORS_SITE = {
@@ -22,8 +22,21 @@ if (sites.length === 0) {
     sites = sitesEnv.dev;
   }
 }
+const matchRegHost = (sites: string[], host: string): boolean => {
+  for (const site of sites) {
+    if (site === '*') {
+      return true;
+    }
+    const r = new RegExp(site);
+    if (r.test(host)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 CORS_SITE.origin = function (origin: string, callback: any) {
-  if (!origin || sites.includes(origin)) {
+  if (!origin || sites.includes(origin) || matchRegHost(sites, origin)) {
     callback(null, true);
   } else {
     console.error('CORS', origin);
