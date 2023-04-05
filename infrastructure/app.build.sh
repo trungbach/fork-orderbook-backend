0d9afd879b7a12111a7e0b25a8fc058594c48da3
im6h <danghaiv.18@gmail.com>
2023-04-05 16:49:28 +0700

    bump version docker image

--- infrastructure/app.build.sh
+++ infrastructure/app.build.sh
@@ -14,7 +14,7 @@ do
             docker build -f ./Dockerfile -t $IMAGE_REPOSITORY ../
         ;;
         "staging")
-            VERSION=0.0.2
+            VERSION=0.0.3
             IMAGE_TAG=oraichain/foundation-orderbook_explorer-stg
             IMAGE_REPOSITORY=$IMAGE_TAG:$VERSION
             echo BUILD IMAGE: $IMAGE_REPOSITORY
