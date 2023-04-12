a935ecd6ec39e89c934077be042e2359084734a9
im6h <danghaiv.18@gmail.com>
2023-04-12 15:14:41 +0700

    test ci/cd release

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,15 @@
 set -ex
 
 echo $INAME
+regex='v[0-9].[0-9]+.[0-9]+$'
 
-echo $DOCKER_TOKEN | docker login -u $DOCKER_USER --password-stdin
-
-docker build -t ${INAME} -f ./infrastructure/Dockerfile .
-docker push ${INAME}
+if [[ ${IVERSION} =~ ${regex} ]]; then
+    gh release create ${IVERSION} --title ${IVERSION}
+    # login with docker
+    echo $DOCKER_TOKEN | docker login -u $DOCKER_USER --password-stdin
+    # build and push docker image to repository
+    docker build -t ${INAME} -f ./infrastructure/Dockerfile .
+    docker push ${INAME}
+else
+    gh release create ${IVERSION} --title ${IVERSION} --prerelease
+fi
