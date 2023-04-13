5c10c878709da053c36f51c890eed29e90897cdd
im6h <danghaiv.18@gmail.com>
2023-04-13 15:30:20 +0700

    chrone: fix ci/cd

--- ci/build.sh
+++ ci/build.sh
@@ -3,12 +3,12 @@
 set -ex
 
 echo $INAME
-echo $PROD
+echo $CURRENT_ENV
 
 regex='v[0-9].[0-9]+.[0-9]+$'
 echo $DOCKER_TOKEN | docker login -u $DOCKER_USER --password-stdin
 
-if [[ ${PROD} -n ]]; then
+build_prod() {
     if [[ ${IVERSION} =~ ${regex} ]]; then
         gh release create ${IVERSION} --title ${IVERSION}
         # login with docker
@@ -18,7 +18,18 @@ if [[ ${PROD} -n ]]; then
     else
         gh release create ${IVERSION} --title ${IVERSION} --prerelease
     fi
-else
+}
+
+build_dev() {
     docker build -t ${INAME} -f ./infrastructure/Dockerfile .
     docker push ${INAME}
-fi
+}
+
+case "${CURRENT_ENV}" in
+    "PROD") 
+        build_prod 
+    ;;
+    "DEV") 
+        build_dev 
+    ;;
+esac
